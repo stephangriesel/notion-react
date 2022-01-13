c6851df474f5274fdf138892114587e007332c99
Stephan <sgriesel@gmail.com>
2022-01-13 07:10:32 +0100

    update & test client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Things to Do</h1>
+
+      <button
+        type="button"
+        onClick={() => {
+          fetch("http://localhost:8000/")
+            .then((response) => response.json())
+            .then((payload) => {
+              console.log(payload)
+            });
+        }}
+      >
+        Fetch List
+      </button>
     </div>
   );
 }
